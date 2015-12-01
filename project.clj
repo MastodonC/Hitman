73b9a65dece6a04946512f19a9f7fe5b364a97ac
Antony Woods <antony@mastodonc.com>
2015-12-01 11:52:20 +0000

    Update deps

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
-(defproject hitman "0.2.0"
+(defproject kixi/hitman "0.2.1"
   :description "a clojure markdown engine"
   :url "http://github.com/rcarmo/Hitman"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [hiccup "1.0.5"]
-                 [instaparse "1.3.2"]]
-  :scm {:name "git"
-        :url "http://github.com/rcarmo/Hitman"}
+                 [instaparse "1.4.1"]]
   :main hitman.core)
